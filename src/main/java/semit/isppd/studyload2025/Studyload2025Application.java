0a775ac1ad1c4001ed2aa37086ba86ba49739514
soft-dev-semit <isppd.semit@gmail.com>
2025-01-15 15:01:48 +0200

    SL25-000 Init commit

---
+++ src/main/java/semit/isppd/studyload2025/Studyload2025Application.java
@@ -0,0 +1,13 @@
+package semit.isppd.studyload2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studyload2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Studyload2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/semit/isppd/studyload2025/Studyload2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package semit.isppd.studyload2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Studyload2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
